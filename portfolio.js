16905258a033f3fcda5ba9b3b971eafed6bcab2f
Luther Attayi Jibrin <jibrinluther@gmail.com>
2025-07-04 10:51:12 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.getElementById("download")
+    .addEventListener("click", ()=>{
+        const pdf = this.document.getElementById("pdf");
+        console.log(pdf);
+        console.log(window);
+        html2pdf().form(pdf).save();
+    })
+}
